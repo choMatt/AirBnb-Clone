79f7e0f61bed23eff086a05c51e4c5a24bd2c153
choMatt <mattseneris18@gmail.com>
2023-06-23 14:28:37 +0800

    added hard coded data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        rating:5.0,
+        reviewCount: 6,
+        country: 'USA',
+        cardTitle: "Life lessons witg  Katie Zaferes",
+        price: "136"
+    },
+    {
+        rating:5.0,
+        reviewCount: 30,
+        country: 'USA',
+        cardTitle: "Learn wedding photography",
+        price: "125"
+    },
+    {
+        rating:4.8,
+        reviewCount: 2,
+        country: 'USA',
+        cardTitle: "Group Mountain Biking",
+        price: "50"
+    }
+]
