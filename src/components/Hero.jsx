108c736970f443ba2efd1c2422ced8dd38c0abcb
choMatt <mattseneris18@gmail.com>
2023-06-23 15:37:24 +0800

    replaced class to className as it is causing an error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@ import PhotoGrid from '../assets/Photo-Grid.png'
 
 function Hero(){
     return (
-    <div class="Hero"> 
-        <img class="Hero__image" src={PhotoGrid}/> 
+    <div className="Hero"> 
+        <img className="Hero__image" src={PhotoGrid}/> 
 
-        <h1 class="Hero__title">
+        <h1 className="Hero__title">
             Online Experiences
         </h1> 
 
-        <h3 class="Hero__subtitle"> 
+        <h3 className="Hero__subtitle"> 
             Join unique interactive activities led by one-of-a-kind  
             hosts—all without leaving home. 
         </h3> 
