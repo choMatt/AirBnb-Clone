0387104377b4052caa9c1f1a1731a707405b4c59
choMatt <mattseneris18@gmail.com>
2023-06-24 06:18:27 +0800

    card rendering logic restored

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,11 +1,22 @@
 import React from "react"
+import CardData from "../data"
+import Card from "../components/Card"
+
 
 function Courses(){
+
+    const cardElements = CardData.map((card) => {
+        return (
+            <Card  
+                coverImg={card.coverImg}
+            />
+        )
+    })
+
     return (
-        <>
-
-
-        </>
+        <div className="courses">
+            {cardElements}
+        </div>
     )
 }
 
