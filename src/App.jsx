c206b5ef804e5658dcbb78ad3cb062873e0c14fe
choMatt <mattseneris18@gmail.com>
2023-06-23 15:34:07 +0800

    imported CardData and Card component and used the map method on our data for the Card component to be rendered dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import Header from "../src/components/Header"
 import Hero from "../src/components/Hero"
+import CardData from "../src/data"
+import Card from "../src/components/Card"
 
 function App() {
+
+  const cardElements = CardData.map((data) => {
+     return(
+      <Card 
+        coverImg={data.coverImg}
+      />
+     )
+  })
 
   return (
     <div>
       <Header />
       <main>
         <Hero />
+        {cardElements}
       </main>
     </div>
   )
