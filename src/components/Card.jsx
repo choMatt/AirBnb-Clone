59137ca4716ca6289bd758d149c3b37a2d9e534f
choMatt <mattseneris18@gmail.com>
2023-06-24 10:15:05 +0800

    finished Card markup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Card(props){
     return (
-        <div className='card'>
-            <img src={`../src/assets/${props.coverImg}`}/>
+        <div className="card">
+            <img className="card__cover-img" src={`../src/assets/${props.coverImg}`} />
+            <div className="card__information">
+                <img className="card__star-icon" src="../src/assets/Star-Icon.png" />
+                <span className="card__rating">{props.rating}</span>
+                <span className="card__review-count">({props.reviewCount})</span>
+                <span className="card__country">{props.country}</span>
+                <p className="card__title">{props.cardTitle}</p>
+                <p className="card__price"><b>From ${props.price}</b> / person</p>
+            </div>
         </div>
     )
 }
